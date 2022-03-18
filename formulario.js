9895bfce7374803e72d9ebe05cd936c563ee4606
Saulo Rodrigues Maia <saulormaia@hotmail.com>
2022-03-18 13:53:34 -0300

    termino da criação do CSS

---
+++ formulario.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const username = document.getElementById("usernameemail");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
