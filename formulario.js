cbf6a271ba05b481b961144673f5cfbee46a8643
Saulo Rodrigues Maia <saulormaia@hotmail.com>
2022-03-18 16:43:48 -0300

    Criando o JS

--- formulario.js
+++ formulario.js
@@ -1,5 +1,50 @@
 const form = document.getElementById("form");
-const username = document.getElementById("usernameemail");
+const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const passwordConfirmationValue = passwordConfirmation.value;
+
+    if (usernameValue === "") {
+        setErrorFor(username, "O nome do usuário é Obrigatório.");
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue ===""){
+        setErrorFor(email, "O email é obrigatório.")
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, "Por favor insira um emial obrigatório");
+    } else {
+        setSuccessFor(email)
+    }
+}
+
+function setErrorFor(input, massege) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small")
+
+    // Adiciona a menssagem de erro
+    small.innerText = message;
+
+    // Adiciona a classe erro
+    formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    // Adicionar a classe de sucesso 
+    formControl.className = "form-control success";
+}
